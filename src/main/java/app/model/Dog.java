f24069fd8475e454a8c7aeba209ccd6b550e1cfc
Sarazhakova <84737903+Sarazhakova@users.noreply.github.com>
2021-12-14 12:09:34 +0300

    Initial commit

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,11 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog {
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
+}
